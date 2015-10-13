c404aecf12d6b24155b757825610e8307d8348b3
Metazion <metazion@outlook.com>
2015-10-13 20:39:31 +0800

    Improve db library

--- src/com/metazion/jm/db/Table.java
+++ src/com/metazion/jm/db/Table.java
@@ -2,10 +2,6 @@ package com.metazion.jm.db;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -27,10 +23,8 @@ public class Table<T> {
 		public int type = 0;
 	}
 
-	T object = null;
-
 	private String tableName = "";
-	TableField primaryField = new TableField();
+	private TableField primaryField = new TableField();
 	private ArrayList<TableField> tableFields = new ArrayList<TableField>();
 
 	private String selectAllSql = "";
@@ -76,8 +70,20 @@ public class Table<T> {
 		deleteSql = generateDeleteSql();
 	}
 
-	public void setObject(T object) {
-		this.object = object;
+	public Record<T> createRecord() {
+		return new Record<T>(this);
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public TableField getPrimaryField() {
+		return primaryField;
+	}
+
+	public ArrayList<TableField> getNoramlFields() {
+		return tableFields;
 	}
 
 	public String getSelectAllSql() {
@@ -98,82 +104,6 @@ public class Table<T> {
 
 	public String getDeleteSql() {
 		return deleteSql;
-	}
-
-	public void setSelectParams(PreparedStatement pst) throws Exception {
-		setPrimaryParams(1, pst);
-	}
-
-	public void setInsertParams(PreparedStatement pst) throws Exception {
-		int start = setPrimaryParams(1, pst);
-		setNormalParams(start, pst);
-	}
-
-	public void setUpdateParams(PreparedStatement pst) throws Exception {
-		int start = setNormalParams(1, pst);
-		setPrimaryParams(start, pst);
-	}
-
-	public void setDeleteParams(PreparedStatement pst) throws Exception {
-		setPrimaryParams(1, pst);
-	}
-
-	public int setPrimaryParams(int start, PreparedStatement pst) throws Exception {
-		Object value = getFieldValue(primaryField);
-		value = toDBValue(primaryField, value);
-		pst.setObject(start, value);
-		return start + 1;
-	}
-
-	public int setNormalParams(int start, PreparedStatement pst) throws Exception {
-		int size = tableFields.size();
-		for (int index = 0; index < size; ++index) {
-			TableField tableField = tableFields.get(index);
-			Object value = getFieldValue(tableField);
-			value = toDBValue(tableField, value);
-			pst.setObject(start + index, value);
-		}
-		return start + size;
-	}
-
-	public void setPrimaryResult(ResultSet rs) throws Exception {
-		Object value = rs.getObject(primaryField.columnName);
-		value = fromDBValue(primaryField, value);
-		setFieldValue(primaryField, value);
-	}
-
-	public void setNormalResult(ResultSet rs) throws Exception {
-		int size = tableFields.size();
-		for (int index = 0; index < size; ++index) {
-			TableField tableField = tableFields.get(index);
-			Object value = rs.getObject(tableField.columnName);
-			value = fromDBValue(tableField, value);
-			setFieldValue(tableField, value);
-		}
-	}
-
-	private Object getFieldValue(TableField tableField) throws Exception {
-		Field field = object.getClass().getDeclaredField(tableField.fieldName);
-		return field.get(object);
-	}
-
-	private void setFieldValue(TableField tableField, Object value) throws Exception {
-		Field field = object.getClass().getDeclaredField(tableField.fieldName);
-		field.set(object, value);
-	}
-
-	private Object toDBValue(TableField tableField, Object value) {
-		if (tableField.type == TableField.TYPE_TIMESTAMP) {
-			value = new Timestamp((long) value);
-		}
-		return value;
-	}
-
-	private Object fromDBValue(TableField tableField, Object value) {
-		if (tableField.type == TableField.TYPE_TIMESTAMP) {
-			value = ((Timestamp) value).getTime();
-		}
-		return value;
 	}
 
 	private String generateSelectAllSql() {
